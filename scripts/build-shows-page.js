97a31b8fff1152bd0862a0e42696785d10656a4d
samihasamin <samihasamin@hotmail.com>
2025-02-01 20:37:06 -0500

    Create shows list using JavaScript DOM manipulation and style in css using mobile first methodology

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,155 @@
+const shows = [
+  {
+    date: "Mon Sept 09 2024",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Tue Sept 17 2024",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Oct 12 2024",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Nov 16 2024",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Friday Nov 29 2024",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Wed Nov 29 2024",
+    venue: "Press Club",
+    location: "San Francisco, CA",
+  },
+];
+
+const showsContainer = document.querySelector(".shows-container");
+
+//Create Header
+
+const showsTitle = document.createElement("h1");
+showsTitle.classList.add("shows-container__title");
+showsTitle.textContent = "Shows";
+showsContainer.appendChild(showsTitle);
+
+shows.forEach((show) => {
+  //Create div for each show block
+
+  const showsEl = document.createElement("div");
+  showsEl.classList.add("shows-container__list");
+
+  //Create Date
+
+  const dateLabel = document.createElement("p");
+  dateLabel.classList.add("shows-container__list--label");
+  dateLabel.textContent = "DATE";
+
+  const dateDetails = document.createElement("p");
+  dateDetails.classList.add("shows-container__list--date");
+  dateDetails.textContent = `${show.date}`;
+
+  //Create Venue
+
+  const venueLabel = document.createElement("p");
+  venueLabel.classList.add("shows-container__list--label");
+  venueLabel.textContent = "VENUE";
+
+  const venueDetails = document.createElement("p");
+  venueDetails.classList.add("shows-container__list--venue");
+  venueDetails.textContent = `${show.venue}`;
+
+  //Create Location
+
+  const locationLabel = document.createElement("p");
+  locationLabel.classList.add("shows-container__list--label");
+  locationLabel.textContent = "LOCATION";
+
+  const locationDetails = document.createElement("p");
+  locationDetails.classList.add("shows-container__list--location");
+  locationDetails.textContent = `${show.location}`;
+
+  //Create Button
+
+  const buyticketButton = document.createElement("button");
+  buyticketButton.classList.add("shows-container__list--button");
+  buyticketButton.textContent = "BUY TICKETS";
+
+  //Append elements to showsContainer and showsEl
+
+  showsEl.appendChild(dateLabel);
+  showsEl.appendChild(dateDetails);
+  showsEl.appendChild(venueLabel);
+  showsEl.appendChild(venueDetails);
+  showsEl.appendChild(locationLabel);
+  showsEl.appendChild(locationDetails);
+  showsEl.appendChild(buyticketButton);
+
+  showsContainer.appendChild(showsEl);
+});
+
+// Tablet and Desktop table container
+
+const table = document.createElement("table");
+table.classList.add("shows-table");
+
+const tableHeader = document.createElement("thead");
+tableHeader.classList.add("shows-table__head");
+
+const tableHeaderRow = document.createElement("tr");
+tableHeaderRow.classList.add("shows-table__head--row");
+
+const showsHeader = ["DATE", "VENUE", "LOCATION"];
+
+showsHeader.forEach((headerContent) => {
+  const labelHeader = document.createElement("th");
+  labelHeader.classList.add("shows-table__head--label");
+
+  labelHeader.textContent = headerContent;
+  tableHeaderRow.appendChild(labelHeader);
+});
+
+tableHeader.appendChild(tableHeaderRow);
+
+const tableBody = document.createElement("tbody");
+tableBody.classList.add("shows-table__body");
+
+shows.forEach((show) => {
+  const row = document.createElement("tr");
+  row.classList.add("shows-table__body--row");
+  const dateCell = document.createElement("td");
+  dateCell.classList.add("shows-table__body--date");
+  dateCell.textContent = show.date;
+  row.appendChild(dateCell);
+
+  const venueCell = document.createElement("td");
+  venueCell.classList.add("shows-table__body--venue");
+  venueCell.textContent = show.venue;
+  row.appendChild(venueCell);
+
+  const locationCell = document.createElement("td");
+  locationCell.classList.add("shows-table__body--location");
+  locationCell.textContent = show.location;
+  row.appendChild(locationCell);
+
+  const buttonCell = document.createElement("td");
+  const button = document.createElement("button");
+  button.textContent = "BUY TICKETS";
+  button.classList.add("shows-table__body--button");
+  buttonCell.appendChild(button);
+  row.appendChild(buttonCell);
+
+  tableBody.appendChild(row);
+});
+
+showsContainer.appendChild(table);
+
+table.appendChild(tableHeader);
+table.appendChild(tableBody);
